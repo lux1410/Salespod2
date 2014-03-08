2f9510e7d7b5d03d23c529d2ff25c9ebeeda9466
lux <tomislav.luketic@sb.htnet.hr>
2014-03-09 00:37:25 +0100

    Initial Commit

---
+++ Salespod2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Salespod2
+//
+//  Created by inin on 3/9/14.
+//  Copyright (c) 2014 lux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Salespod2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Salespod2
+//
+//  Created by inin on 3/9/14.
+//  Copyright (c) 2014 lux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
